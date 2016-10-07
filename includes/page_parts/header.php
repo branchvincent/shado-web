21602bca776ab3e78c12ed5f289f0091fd9f34da
branchvincent <branchevincent@gmail.com>
2016-10-07 16:04:58 -0400

    Reorganizing files

--- includes/page_parts/header.php
+++ includes/page_parts/header.php
@@ -4,10 +4,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?php if (isset($page_title)) echo $page_title; else echo 'SHOW';?></title>
 		<link rel="stylesheet" type="text/css" href="styles/global_styles.css.php">
-		<!-- <link rel="stylesheet" type="text/css" href="tooltips/tooltip.css"> -->
-		<link rel="stylesheet" type="text/css" href="tooltips/new_tooltip.css">
+		<link rel="stylesheet" type="text/css" href="styles/tooltip.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-		<!-- <script type="text/javascript" src="tooltips/tooltip.js"></script> -->
 		<script type="text/javascript" src="scripts/graph_navBar.js"></script>
 		<script type="text/javascript" src="scripts/nav_selections.js"></script>
 		<?php if (isset($html_head_insertions)) echo $html_head_insertions;?>
