85267f3375d23f850b9cdf10547b532715290f1c
branchvincent <branchevincent@gmail.com>
2017-01-27 10:11:50 -0500

    missing pic

--- view_detailed_results.php
+++ view_detailed_results.php
@@ -1,37 +1,36 @@
 <?php
 	require_once('includes/php_session/init.php');
+
 	$html_head_insertions = '<script src="http://d3js.org/d3.v3.min.js"></script>' . "\r\n\t\t";
 	$html_head_insertions .= '<script type="text/javascript" src="includes/results/d3_graph.js"></script>';
 	$page_title = 'Detailed Analysis';
 	require_once('includes/page_parts/header.php');
 	require_once('includes/page_parts/side_navigation.php');
 	require_once('includes/results/graph_navBar_calculations.php');
-	// require_once('includes/results/d3_graph.php');
-	require_once('includes/results/graph_CsvFile.php');
-	echo "<div id='page' class='page'>";
-
-	if ($_GET['operator'] == 'conductor') {
-		$assistant = "conductor";
-		require_once('includes/results/d3_graph.php');
-		createGraphCsv('conductor');
-		graphText($_SESSION['session_dir'] . 'stats_conductor.csv');
-		echo "</div>";
-	} else if ($_GET['operator'] == 'engineer') {
-		$assistant = "Engineer";
-		require_once('includes/results/d3_graph.php');
-		createGraphCsv('engineer');
-		graphText($_SESSION['session_dir'] . 'stats_engineer.csv');
-		echo "</div>";
-	} else if ($_GET['operator'] == 'dispatcher') {
-		$assistant = "Dispatcher";
-		require_once('includes/results/d3_graph.php');
-		createGraphCsv('dispatcher');
-		graphText($_SESSION['session_dir'] . 'stats_dispatcher.csv');
-		echo "</div>";
-	} else {
-		die('There was an error');
-	}
 ?>
+
+<?php if ($_GET != 'dispatcher'):
+	<div class="centerOuter">
+		<div class="startEndTime stepBox" style="width: 220px;">
+			<!-- <div class='stepCircle'>1</div> -->
+			<h3 class='whiteFont'>
+				What Batch Do You Want to See?
+				<?=Util::createTooltip('Enter the batch number.')?>
+			</h3>
+
+			<select>
+				<?=Util::getSelectOptions(range(1,3), 1)?>
+			</select>
+		</div>
+	</div>
+<?php endif?>
+
+	<div class="centerOuter">
+		<div style="width: 220px;">
+			
+		</div>
+	</div>
+
 	<div id="bottomNav">
 		<ul>
 			<li>
@@ -40,9 +39,9 @@
 			<li>
 				<button type="button" class="button" onclick="location.href='sim_summary.php';" style="color: black; visibility: hidden;">Print Report</button>
 			</li>
-			<li>
+			<!-- <li>
 				<button type="button" class="button" onclick="location.href='sim_summary.php';" style="color: black;">Preview Report &#8680</button>
-			</li>
+			</li> -->
 		</ul>
 	</div>
 
